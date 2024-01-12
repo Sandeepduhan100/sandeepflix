8c8914d860e851b0598d930248b22e20b9f364fb
unknown <sandeepduhan1432@gmail.com>
2024-01-12 01:26:09 -0800

    const nextConfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir : true,
-    serverComponentsExternalPackages:["mongoose"]
-  }
-}
 
-module.exports = nextConfig
-// next.config.js
 
 module.exports = {
     images: {
